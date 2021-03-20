d23e4dcfc993a0934bd9747a749e46f62251a330
jakubjablonsky <jakub@clientup.io>
2021-03-20 18:12:22 +0100

    Finished Quiz Project v1.0

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # quiz-project-1
+ 
+ 
  Random Quiz Generator
+
+- Generates 5 random quotes from famous people
+- Quotes change by clicking the button or automatically in 5 seconds interval
--- js/script.js
+++ js/script.js
@@ -3,31 +3,85 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+/*
+Array that stores the quotes that are printed to a webpage
+*/
 
-/*** 
- * `quotes` array 
-***/
+let quotes = [
 
+  {
+   quote: "Common Sense is that which judges the things given to it by other senses.", 
+   source: "Leonardo Da Vinci", 
+   year: 1501
+  },
+  {
+    quote: "There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle.", 
+    source: "Albert Einstein", 
+    year: 1944
+   },
+   {
+    quote: "Physics isn’t the most important thing. Love is.", 
+    source: "Richard Feynman", 
+    year: 1980,
+    tags: " #love #priorities #physics #philosophy"
+   },
 
+   {
+    quote: "The greatest enemy of knowledge is not ignorance, it is the illusion of knowledge.", 
+    source: "Daniel J. Boorstin", 
+    citation: "The Washington Post", 
+    year: 1984
+   },
+   {
+    quote: "A man who dares to waste one hour of time has not discovered the value of life.", 
+    source: "Charles Darwin",  
+    citation: "The Life & Letters of Charles Darwin", 
+    year: 1869
+   },
+   
+];
 
-/***
- * `getRandomQuote` function
-***/
+/*
+Function to get a random quote from array of quotes
+*/
 
+function getRandomQuote () {
+  let randomNumber = Math.floor(Math.random() * (quotes.length));
+  getRandomQuotes = quotes[randomNumber];
+  return (getRandomQuotes);
+};
+getRandomQuote();
 
+/*
+Function that prints the quote and updates the HTML 
+It also checks if randomly generated quote, has citation, year or tag property
+*/
 
-/***
- * `printQuote` function
-***/
+function printQuote() {
+  let randomQuote = getRandomQuote();
+  let htmlString = `<p class="quote">${randomQuote.quote}</p>
+                    <p class="source">${randomQuote.source}`
+  
+  if (randomQuote.citation) {
+  htmlString += `<span class="citation">${randomQuote.citation}</span>`
+  }
 
+  if (randomQuote.year) {
+    htmlString += `<span class="year">${randomQuote.year}, </span>`
+  }
 
+  if (randomQuote.tags) {
+    htmlString += `<span class="tags">${randomQuote.tags}</span>`
+  }
 
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
+  htmlString += `</p>`
+
+  document.getElementById('quote-box').innerHTML = htmlString; 
+}
+
+/*
+Interval to print new quote every 5 seconds
+*/
+setInterval(printQuote, 5000);
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
